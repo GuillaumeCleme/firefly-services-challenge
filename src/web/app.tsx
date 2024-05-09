068a487edd80a87903bfc7117051864e0f9f7e87
Guillaume Clement <guillaumecle.me@gmail.com>
2024-05-08 21:43:57 -0600

    Bump dependencies, add redux store, and implement router

---
+++ src/web/app.tsx
@@ -0,0 +1,40 @@
+/* 
+* <license header>
+*/
+
+import React from 'react'
+import { Provider, defaultTheme, Grid, View } from '@adobe/react-spectrum'
+import { HashRouter as Router } from 'react-router-dom'
+import SideBar from './components/SideBar'
+
+import AppRouter from "./router/app-router";
+
+function App (props: Record<any, any>) {
+
+  return (
+      <Router>
+        <Provider theme={defaultTheme} colorScheme={'light'}>
+          <Grid
+            areas={['sidebar content']}
+            columns={['256px', '3fr']}
+            rows={['auto']}
+            height='100vh'
+            gap='size-100'
+          >
+            <View
+              gridArea='sidebar'
+              backgroundColor='gray-200'
+              padding='size-200'
+            >
+              <SideBar></SideBar>
+            </View>
+            <View gridArea='content' padding='size-200'>
+              <AppRouter />
+            </View>
+          </Grid>
+        </Provider>
+      </Router>
+  )
+}
+
+export default App
